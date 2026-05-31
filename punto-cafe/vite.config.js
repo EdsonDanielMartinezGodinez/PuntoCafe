10c9a63ebb8e167a516278977a095850ca5b1289
Edson Daniel Martinez Godinez <128575625+EdsonDanielMartinezGodinez@users.noreply.github.com>
2026-05-31 16:57:14 -0600

    Correcion de error

--- punto-cafe/vite.config.js
+++ punto-cafe/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PuntoCafe/', // required for GitHub Pages project site
   plugins: [react()],
 })
